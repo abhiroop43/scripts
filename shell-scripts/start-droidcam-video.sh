06efde0f82960434aa9fee4a8625e61a68c53bfa
Abhiroop Santra <abhiroop.santra@gmail.com>
2022-01-04 23:54:29 +0400

    added script to start droidcam video

---
+++ shell-scripts/start-droidcam-video.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+PHONE_IP=$1
+PHONE_PORT=$2
+echo "Starting Droidcam process with Audio only with IP $PHONE_IP and Port $PHONE_PORT"
+
+droidcam-cli -a -v $PHONE_IP $PHONE_PORT &
+
+echo "Droidcam client is started"
+
+echo "Enabling sound...."
+
+LOOPBACK_ID=$(grep -F "[Loopback" < /proc/asound/cards | awk '{print $1}')
+
+echo "Loopback Id found to be $LOOPBACK_ID"
+
+pacmd load-module module-alsa-source device=hw:Loopback,1,$LOOPBACK_ID
+
+echo "Sound enabled"
